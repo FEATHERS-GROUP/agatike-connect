db020bf7226a7145b2bf2b01dab7871f9f3465f7
Abeltony-prog <abeltony03@gmail.com>
2026-06-21 21:42:16 +0200

    contrbutor to agatike studio added

--- test-db.ts
+++
@@ -1,7 +0,0 @@
-import { hasuraRequest } from "./src/api/hasura";
-async function run() {
-  const q = `query { workspace_users(where: { email: { _ilike: "%colleague%" } }) { id email workspaces } }`;
-  const res = await hasuraRequest(q, {});
-  console.log(res);
-}
-run();
--- test-schema.js
+++
@@ -1,3 +0,0 @@
-const { hasuraRequest } = require("./dist/api/graphql.server.js"); // Wait, it's not compiled
-
-// Let's use fetch with the env vars
