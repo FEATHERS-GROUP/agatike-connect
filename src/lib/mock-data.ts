fedfe7eb5e16918a146dfe792652e265348a3171
Abeltony-prog <abeltony03@gmail.com>
2026-07-12 13:53:08 +0200

    UI changes for the user not loggedin

---
+++ src/lib/mock-data.ts
@@ -0,0 +1,7 @@
+export type Event = any;
+export const events: any[] = [{}];
+export const experiences: any[] = [{}];
+export const movies: any[] = [{}];
+export const ticketTiers: any[] = [];
+export const merch: any[] = [];
+export const experienceCategories: string[] = [];
